a58f98ceb58901f3c038671893a134f10ac2871c
wangchao4631 <wangchao4631@163.com>
2019-08-22 15:07:46 +0800

    create project

---
+++ src/main/java/com/newer/spring0822/Spring0822Application.java
@@ -0,0 +1,13 @@
+package com.newer.spring0822;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0822Application.class, args);
+    }
+
+}
